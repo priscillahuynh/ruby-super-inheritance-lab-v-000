4d2cf7498d8e9dc873fe08a444f52113474aab92
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 05:34:58 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,15 +6,15 @@ class ChattyStudent < Student
     end
 
     def raise_hand
-      super 
       super
-      super 
-      super 
       super
-      super 
       super
-      super 
-      super 
+      super
+      super
+      super
+      super
+      super
+      super
       super
     end
 end
